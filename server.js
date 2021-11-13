f6690f9343ae11d4cb6e7b4d4a075e065ae76ef6
Chikanma <chikanmaonuegbu@gmail.com>
2021-11-12 18:04:19 -0700

    slight changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-
+const path = require('path');
 const items = require('./routes/api/items')
 const app = express();
 
@@ -20,6 +20,16 @@ mongoose
 
 // Use Routes
 app.use('/api/items',items)
+
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+//Port
 const port = process.env.port || 5000;
 
 
